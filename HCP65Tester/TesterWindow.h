f42bb15683ca08c8676fc63764e9b20fa0afb4ca
Andrew Paterson <andrew.ian.paterson@gmail.com>
2025-10-29 19:04:50 +0200

    Removed a whole lot of unused includes.

--- HCP65Tester/TesterWindow.h
+++ HCP65Tester/TesterWindow.h
@@ -1,20 +1,5 @@
 #ifndef __TESTER_WINDOW_H__
 #define __TESTER_WINDOW_H__
-#include <time.h>
-#include "BaseLib/Define.h"
-#include "BaseLib/Logger.h"
-#include "BaseLib/Numbers.h"
-#include "BaseLib/FileUtil.h"
-#include "BaseLib/CSVHelper.h"
-#include "BaseLib/Version.h"
-#include "BaseLib/DiskFile.h"
-#include "BaseLib/BufferedFile.h"
-#include "BaseLib/Numbers.h"
-#include "BaseLib/MD5.h"
-#include "BaseLib/TextParser.h"
-#include "BaseLib/WinInit.h"
-#include "BaseLib/WinConfig.h"
-#include "BaseLib/PropertiesFile.h"
 #include "WindowLib/Window.h"
 
 
